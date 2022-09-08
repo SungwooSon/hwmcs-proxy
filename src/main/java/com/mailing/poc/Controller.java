5354095405e53fe91fa18bfed7dc1733ed5e4609
SungwooSon <ssw0418@kakao.com>
2022-09-08 11:41:12 +0900

    가상계정 형식 변경

--- src/main/java/com/mailing/poc/Controller.java
+++ src/main/java/com/mailing/poc/Controller.java
@@ -33,8 +33,8 @@ public class Controller {
             userIdMap.put("julien"+i, 14);
         }
 
-        userIdMap.put("flowly11@test.com", 63);
-        userIdMap.put("flowly12@test.com", 64);
+        userIdMap.put("flowly11", 63);
+        userIdMap.put("flowly12", 64);
     }
 
     @GetMapping(value = "/flowly/stress")
