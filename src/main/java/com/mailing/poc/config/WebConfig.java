1ae773aecfe1b81aed78da7ec13b91dcc0b53119
SungwooSon <ssw0418@kakao.com>
2022-11-09 16:58:07 +0900

    cors 에러 대응

--- src/main/java/com/mailing/poc/config/WebConfig.java
+++ src/main/java/com/mailing/poc/config/WebConfig.java
@@ -10,8 +10,6 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://admin.mcspoc.net")
-                .allowedOrigins("*")
                 .allowedMethods(
                         HttpMethod.GET.name(),
                         HttpMethod.POST.name(),
@@ -21,14 +19,5 @@ public class WebConfig implements WebMvcConfigurer {
                         HttpMethod.HEAD.name(),
                         HttpMethod.OPTIONS.name()
                 );
-//                .allowedOrigins("http://localhost:3000"
-//                               ,"http://localhost:3001"
-//                               ,"http://192.168.0.17:3000"    //
-//                               ,"http://222.232.242.112:3000" // gbsoft 공인 ip
-//                               ,"http://211.215.6.105:3000"   // 새싹 집 ip
-//                               ,"http://211.215.6.105:3001"   // 새싹 집 ip
-//                                );
-//                                //하나씩 다 입력해줘야되? 이건 아니지..
-
     }
 }
